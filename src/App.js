fcbad1a3f33b951789e9a17f74136a92ce930c94
ayushgupta2959 <ayushgupta2959@gmail.com>
2024-05-25 02:33:04 +0530

    added navigation, search

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/Landify.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await provider.listAccounts();
+      setAccount(accounts[0]);
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
-
-      <div className='cards__section'>
-
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
+      <div className="cards__section">
         <h3>Welcome to Millow</h3>
-
       </div>
-
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,43 @@
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
+import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.listAccounts();
+    setAccount(accounts[0]);
+  };
 
-}
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#">Buy</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Sell</a>
+        </li>
+      </ul>
+
+      <div className="nav__brand">
+        <img src={logo} alt="logo" />
+        <h1>Landify</h1>
+      </div>
+
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6)}...{account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
